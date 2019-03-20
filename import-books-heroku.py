9e4f9c4fafd7f829630e68bbd8ab4646655806f5
SashaRadovic <sasa.a.radovic@gmail.com>
2019-03-20 06:52:21 +0100

    Update import-books-heroku.py

--- import-books-heroku.py
+++ import-books-heroku.py
@@ -3,7 +3,7 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-os.environ["DATABASE_URL"]="postgres://yxhgpatjczqebb:7b840aada0efcb2afc3b315236a7753c7a5537772fbfc3451bea67f53d9e825e@ec2-50-19-109-120.compute-1.amazonaws.com:5432/d8v6ci2dq60tlp"
+os.environ["DATABASE_URL"]="DATABASE_URL"
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 db.execute("CREATE TABLE books(id SERIAL, isbn VARCHAR PRIMARY KEY NOT NULL, title VARCHAR NOT NULL, author VARCHAR, year varchar(4) )")
